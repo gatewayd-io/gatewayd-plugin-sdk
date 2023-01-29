951c980a06a35f36cb6c4f4cc613c1b863231fe8
Mostafa Moradian <mostafa@gatewayd.io>
2023-01-29 16:23:59 +0100

    Add GetLogLevel function

---
+++ logging/utils.go
@@ -0,0 +1,23 @@
+package logging
+
+import "github.com/hashicorp/go-hclog"
+
+// GetLogLevel returns the hclog level based on the string passed in.
+func GetLogLevel(logLevel string) hclog.Level {
+	switch logLevel {
+	case "trace":
+		return hclog.Trace
+	case "debug":
+		return hclog.Debug
+	case "info":
+		return hclog.Info
+	case "warn":
+		return hclog.Warn
+	case "error":
+		return hclog.Error
+	case "off":
+		return hclog.Off
+	default:
+		return hclog.NoLevel
+	}
+}
