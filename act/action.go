6f2765052603d3ccf0980c49343a95ea9c3f080c
Mostafa Moradian <mostafa@gatewayd.io>
2024-02-25 15:49:05 +0100

    Use optional params instead of named ones

--- act/action.go
+++ act/action.go
@@ -1,9 +1,16 @@
 package act
 
-import "github.com/rs/zerolog"
+// Parameter is a parameter that can be passed to an action by the system (runner),
+// like a logger, a database connection, or any other data.
+type Parameter struct {
+	Key   string
+	Value any
+}
 
-// ActionFunc is a function that can be used to run an action.
-type ActionFunc func(logger zerolog.Logger, data map[string]any) (any, error)
+// ActionFunc is a function that can be used to run an action. Optionally, it can
+// receive parameters like a logger, a database connection, or any other data that
+// the action needs to run.
+type ActionFunc func(data map[string]any, params ...Parameter) (any, error)
 
 // Action is any action that can be performed by the system.
 type Action struct {
