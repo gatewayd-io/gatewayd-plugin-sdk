996d55c45a8524d90e66c954b36d37678b40bb99
Mostafa Moradian <mostafa@gatewayd.io>
2024-02-25 02:37:21 +0100

    Add helper functions to generate signals

--- act/signal.go
+++ act/signal.go
@@ -11,3 +11,34 @@ func (s *Signal) ToMap() map[string]any {
 		Metadata: s.Metadata,
 	}
 }
+
+func Terminate() *Signal {
+	return &Signal{
+		Name: "terminate",
+		Metadata: map[string]any{
+			"terminate": true,
+		},
+	}
+}
+
+func Log(level, message string, fields map[string]any) *Signal {
+	metadata := map[string]any{
+		"log":     true,
+		"level":   level,
+		"message": message,
+	}
+
+	if fields != nil {
+		for k, v := range fields {
+			if k == "level" || k == "message" {
+				continue
+			}
+			metadata[k] = v
+		}
+	}
+
+	return &Signal{
+		Name:     "log",
+		Metadata: metadata,
+	}
+}
